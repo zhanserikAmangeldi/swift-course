6da9945baace777e2627fc2866cbb33a29665f6a
zhanserikAmangeldi <amangeldi.janserik2017@gmail.com>
2025-01-24 16:58:55 +0500

    Hello, World!

---
+++ main.swift
@@ -0,0 +1,10 @@
+
+print("Hello world!")
+
+let name: String = "Zhanserik"
+var surname: String = "Amangeldi"
+// name = "Alikhan" arise error because name is const variable
+surname = "Amangeldi_V2"
+
+let years: Int = 19
+print("My name is \(name), and surname is \(surname), and I am \(years) years old.")
